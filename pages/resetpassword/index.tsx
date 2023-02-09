33658e6497b3b9828c1441e5cbe35a1b66829a0f
Aryan Kumar <aryan.kumar@squareboat.com>
2023-02-10 01:49:47 +0530

    FIX:Reset Password Error

--- pages/resetpassword/index.tsx
+++ pages/resetpassword/index.tsx
@@ -62,8 +62,8 @@ const Index = () => {
       <div className="bg-dark-blue w-full h-[40vh]  2xl:h-[16vh] text-white flex items-center justify-center">
         <div className="mainWrapper">
           <div className="md:w-[557px] w-[450px] xs:w-[310px] h-[420px] bg-white box-shadows rounded-[20px] mt-[21rem] flex flex-col items-center">
-            <div className="text-light-dark w-full py-4 px-7 ">
-              <h1 className="text-light-dark text-[22px] font-medium py-4 xs:py-2 px-0">
+            <div className="text-light-dark w-full py-4 px-7  ">
+              <h1 className="text-light-dark text-[22px] font-medium py-4 xs:py-2 px-0  ">
                 Reset Your Password
               </h1>
               <h3 className="text-[14px] text-light-dark pb-4">
@@ -81,14 +81,13 @@ const Index = () => {
                 error={error}
                 required
                 onBlur={() => {}}
-              />
-                 {error ? (
+              >
+                {error &&
                   <p className="text-[#FF0000] text-right text-xs">
                     Password do not Match.
                   </p>
-                ) : (
-                  ""
-                )}
+                }
+              </Fields>
               <Fields
                 type="password"
                 content="Confirm new password"
@@ -102,17 +101,15 @@ const Index = () => {
                 required
                 onBlur={() => {}}
               >
-                {error ? (
+                {error &&
                   <p className="text-[#FF0000] text-right text-xs">
                     Password do not Match.
                   </p>
-                ) : (
-                  ""
-                )}
+                }
               </Fields>
               <div className="flex items-center justify-center">
                 <button
-                  className="w-40 h-[46px] bg-light-blue border text-[16px] font-medium  border-light-blue rounded-md  flex items-center justify-center mt-8 cursor-pointer text-[#fff]"
+                  className="w-40 h-[46px] bg-light-blue border text-[16px] font-medium  border-light-blue rounded-md  flex items-center justify-center mt-6 cursor-pointer text-[#fff]"
                   onClick={onResetPassword}
                   disabled={isLoading}
                   type="submit"
