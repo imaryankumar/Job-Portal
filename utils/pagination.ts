a2136878d019a532ba214ac9070b707412987b5a
Aryan Kumar <aryan.kumar@squareboat.com>
2023-02-10 12:36:48 +0530

    chors:Pagination.tsx file change

--- utils/pagination.ts
+++ utils/pagination.ts
@@ -1,4 +1,4 @@
-export const increment = ({count,setCount, totalPage,router }) => {
+export const increment = ({count,setCount, totalPage,router }:any) => {
     if (count < totalPage) {
       count < totalPage && setCount(count + 1);
       router.push(`/jobs-posted-by-you?page=${count + 1}`, undefined, {
