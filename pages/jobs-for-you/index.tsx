ba81c6042e755eb748d17e1726510e9f79dbcb39
Aryan Kumar <aryan.kumar@squareboat.com>
2023-02-11 00:48:36 +0530

    fix: jobs for you

--- pages/jobs-for-you/index.tsx
+++ pages/jobs-for-you/index.tsx
@@ -29,14 +29,14 @@ const Index = () => {
   const [loader, setLoader] = useState(false);
 
   const { user } = useContext(authcontext);
-  const pageNum=router.asPath?.split('=')[1]
+  const pageNum = router.asPath?.split("=")[1];
   useEffect(() => {
-    const page =  pageNum ? +pageNum : 1;
+    const page = pageNum ? +pageNum : 1;
     if (!isNaN(page) && page > 0) {
       setCount(page);
       reloadData(page);
     }
-  }, [router,pageNum]);
+  }, [router, pageNum]);
 
   let myArray = useMemo(() => {
     if (!isNaN(totalPage)) {
@@ -83,10 +83,9 @@ const Index = () => {
   const pageDefiner = (num: any) => {
     if (num > 2) {
       return [num - 2, num - 1, num];
-    }else if(num==1){
+    } else if (num == 1) {
       return [num];
-    }
-     else {
+    } else {
       return [num - 1, num];
     }
   };
@@ -106,7 +105,9 @@ const Index = () => {
   const decrement = () => {
     if (count > 1) {
       count == 1 ? setCount(1) : setCount(count - 1);
-      router.push(`/jobs-for-you?page=${count - 1}`, undefined, { shallow: true });
+      router.push(`/jobs-for-you?page=${count - 1}`, undefined, {
+        shallow: true,
+      });
       window.scroll({
         top: 0,
         left: 0,
@@ -185,153 +186,152 @@ const Index = () => {
             </div>
           </div>
           <div>
-              {loader ? (
-                <Loader />
-              ) : (
-              <div className="absloute" >
-                 <div  className="flex flex-wrap items-center gap-[2%] mainWrapper justify-center md:justify-start md:px-40 xs:px-1 px-8" >
-                {
-                   myCanData?.map((item: cardTypes, key) => {
-                    return (
-                      <div
-                        className="w-[80%] sm:w-[32%] md:w-[49%] lg:w-[23%] h-[180px] bg-white rounded mb-4 px-4 py-4 relative capitalize shadow"
-                        key={key}
-                      >
+            {loader ? (
+              <Loader />
+            ) : (
+              <div className="relative">
+                <div className="min-h-[80vh] pb-20 ">
+                  <div className="flex md:mx-40 2xl:mx-96 justify-center md:justify-start gap-4 flex-wrap ">
+                    {myCanData?.map((item: cardTypes, key) => {
+                      return (
                         <div
-                          className={`text-[17px] text-light-dark tracking-normal line-clamps break-word  `}
+                          className="w-[80%] sm:w-[32%] md:w-[49%] lg:w-[23%] h-[180px] bg-white rounded mb-4 px-4 py-4 relative capitalize shadow"
                           key={key}
-                          title={item.title}
-                          data-toggle="tooltip"
                         >
-                          <h1>{item.title}</h1>
+                          <div
+                            className={`text-[17px] text-light-dark tracking-normal line-clamps break-word  `}
+                            key={key}
+                            title={item.title}
+                            data-toggle="tooltip"
+                          >
+                            <h1>{item.title}</h1>
+                          </div>
+                          <div
+                            className={` text-[14px]  tracking-normal text-light-dark opacity-80 my-2 mx-0  break-words line-clamp `}
+                            title={item.description}
+                            data-toggle="tooltip"
+                          >
+                            <p>{item.description}</p>
+                          </div>
+                          <div className="absolute left-4 bottom-5 grid grid-cols-6">
+                            <div className="grid grid-cols-12 content-center col-span-4">
+                              <div className="col-span-1">
+                                <Image
+                                  src="/iconsimgs/location.svg"
+                                  alt="Pinicons"
+                                  width={15}
+                                  height={10}
+                                  className=" object-contain "
+                                />
+                              </div>
+                              <div className="col-span-8 ml-2">
+                                <p
+                                  className={`text-[14px] tracking-normal break-all text-light-dark opacity-80 line-clamps`}
+                                  title={item.location}
+                                  data-toggle="tooltip"
+                                >
+                                  {item.location}
+                                </p>
+                              </div>
+                            </div>
+                            <div className="col-span-2">
+                              {loader ? (
+                                <Loader />
+                              ) : (
+                                <button
+                                  className="px-4 py-2 bg-[#43afff33] rounded  cursor-pointer text-light-dark capitalize text-[12px]  "
+                                  onClick={() => clickMe(item.id)}
+                                >
+                                  Apply
+                                </button>
+                              )}
+                            </div>
+                          </div>
                         </div>
-                        <div
-                          className={` text-[14px]  tracking-normal text-light-dark opacity-80 my-2 mx-0  break-words line-clamp `}
-                          title={item.description}
-                          data-toggle="tooltip"
-                        >
-                          <p>{item.description}</p>
-                        </div>
-                        <div className="absolute left-4 bottom-5 grid grid-cols-6">
+                      );
+                    })}
+                  </div>
+                  <div className="absolute bottom-0 left-0 right-0 ">
+                    <div className="flex justify-center text-center items-center gap-[1%]  cursor-pointer py-4  ">
+                      <Image
+                        src="/iconsimgs/Prev.svg"
+                        alt="Lefticon"
+                        onClick={() => decrement()}
+                        width={30}
+                        height={30}
+                        className={count == 1 ? "cursor-no-drop" : ""}
+                      />
+                      {count > 1 ? (
+                        <>
+                          <div
+                            className="w-8 h-8 rounded bg-white text-center text-[19px] font-[400] text-black "
+                            onClick={() => onNumClick(1)}
+                          >
+                            1
+                          </div>
+                          <span className="text-black">...</span>
+                        </>
+                      ) : (
+                        ""
+                      )}
+                      {(count + 2 >= totalPage
+                        ? pageDefiner(totalPage)
+                        : [count, count + 1, count + 2]
+                      )?.map((i, k) => {
+                        return (
+                          <span
+                            className="w-8 h-8 rounded bg-[#43afff33] text-center text-[19px] font-[400] text-black "
+                            onClick={() => onNumClick(i)}
+                            style={
+                              count === i
+                                ? {
+                                    color: "black",
+                                    backgroundColor: "#43AFFF33",
+                                    cursor: "pointer",
+                                  }
+                                : {
+                                    backgroundColor: "white",
+                                    cursor: "pointer",
+                                  }
+                            }
+                            key={k}
+                          >
+                            {i}
+                          </span>
+                        );
+                      })}
+                      {count + 2 >= totalPage ? (
+                        ""
+                      ) : (
+                        <>
+                          <span className="text-black">...</span>
+                          <div
+                            className="w-8 h-8 rounded bg-white text-center text-[19px] font-[400] text-black "
+                            onClick={() => onNumClick(totalPage)}
+                          >
+                            {totalPage}
+                          </div>
+                        </>
+                      )}
 
-                        <div className="grid grid-cols-12 content-center col-span-4">
-                     <div className="col-span-1">
-                     <Image
-                            src="/iconsimgs/location.svg"
-                            alt="Pinicons"
-                            width={15}
-                            height={10}
-                            className=" object-contain "
-                          />
-                     </div>
-                          <div className="col-span-8 ml-2">
-                          <p className={`text-[14px] tracking-normal break-all text-light-dark opacity-80 line-clamps`}
-                           title={item.location}
-                           data-toggle="tooltip"
-                          >{item.location}</p>
-                          </div>
-                       
-                      </div>
-                      <div className="col-span-2">
-                            {loader ? (
-                              <Loader />
-                            ) : (
-                              <button
-                                className="px-4 py-2 bg-[#43afff33] rounded  cursor-pointer text-light-dark capitalize text-[12px]  "
-                                onClick={() => clickMe(item.id)}
-                              >
-                                Apply
-                              </button>
-                            )}
-                          </div>
-                        </div>
-                       
-                      </div>
-                    );
-                  })
-                }
-               </div>
-               <div className="absolute left-0 right-0 " >
-               <div className="flex justify-center text-center items-center gap-[1%]  cursor-pointer py-4  ">
-          <Image
-            src="/iconsimgs/Prev.svg"
-            alt="Lefticon"
-            onClick={() => decrement()}
-            width={30}
-            height={30}
-            className={count==1?'cursor-no-drop' :""}
-          />
-          {count > 1 ? (
-            <>
-              <div
-                className="w-8 h-8 rounded bg-white text-center text-[19px] font-[400] text-black "
-                onClick={() => onNumClick(1)}
-              >
-                1
+                      <Image
+                        src="/iconsimgs/Nex.svg"
+                        alt="Righticon"
+                        onClick={() => increment()}
+                        width={30}
+                        height={30}
+                        className={count == totalPage ? "cursor-no-drop" : ""}
+                      />
+                      {}
+                    </div>
+                  </div>
+                </div>
               </div>
-             <span className="text-black">...</span>
-            </>
-          ) : (
-            ""
-          )}
-          {(count + 2 >= totalPage
-            ? pageDefiner(totalPage)
-            : [count, count + 1, count + 2]
-          )?.map((i, k) => {
-            return (
-              <span
-                className="w-8 h-8 rounded bg-[#43afff33] text-center text-[19px] font-[400] text-black "
-                onClick={() => onNumClick(i)}
-                style={
-                  count === i
-                    ? {
-                        color: "black",
-                        backgroundColor: "#43AFFF33",
-                        cursor: "pointer",
-                      }
-                    : { backgroundColor: "white", cursor: "pointer" }
-                }
-                key={k}
-              >
-                {i}
-              </span>
-            );
-          })}
-          {count + 2 >= totalPage ? (
-            ""
-          ) : (
-            <>
-             <span className="text-black">...</span>
-              <div
-                className="w-8 h-8 rounded bg-white text-center text-[19px] font-[400] text-black "
-                onClick={() => onNumClick(totalPage)}
-              >
-                {totalPage}
-              </div>
-            </>
-          )}
-   
-          <Image
-            src="/iconsimgs/Nex.svg"
-            alt="Righticon"
-            onClick={() => increment()}
-            width={30}
-            height={30}
-            className={count==totalPage?'cursor-no-drop' :""}
-           
-          />
-            {}
-        </div>
-               </div>
-              </div>
-              )}
-            
+            )}
           </div>
         </div>
       </div>
-      <div className="bg-white-blue ">
-       
-      </div>
+      <div className="bg-white-blue "></div>
     </>
   );
 };
